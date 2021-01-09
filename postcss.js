cbf5449116b36f46b72b2ad95e50e3b2d189df3d
Ralf S. Engelschall <rse@engelschall.com>
2021-01-09 12:03:57 +0100

    upgrade to PostCSS 8 world-order

--- postcss.js
+++ postcss.js
@@ -22,11 +22,10 @@
 **  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-const postcss               = require("postcss")
 const postcssSelectorParser = require("postcss-selector-parser")
 
 /*  PostCSS Plugin SPI  */
-module.exports = postcss.plugin("postcss-scope", (options = {}) => {
+module.exports = (options = {}) => {
     /*  default options  */
     options = Object.assign({}, {
         atDirective:    "scope",
@@ -36,93 +35,98 @@ module.exports = postcss.plugin("postcss-scope", (options = {}) => {
     }, options)
 
     /*  provide tree processor  */
-    return (css /*, result */) => {
-        /*  list of nodes to remove  */
-        var remove = []
+    return {
+        postcssPlugin: "postcss-scope",
+        Once (css /*, result */) {
+            /*  list of nodes to remove  */
+            var remove = []
 
-        /*  walk down a single CSS node  */
-        const walkNode = (node, scope) => {
-            var scopeLocal = scope
-            if (typeof node === "object" && node.type === "atrule" && node.name === options.atDirective) {
-                /*  @<directive> <id>  */
-                var arg = node.params.replace(/^["']/, "").replace(/['"]$/, "")
-                if (arg === "")
-                    throw node.error(`postcss-scope: WARNING: missing scope argument on directive @${options.atDirective}`)
-                if (typeof node.nodes === "object" && node.nodes instanceof Array && node.nodes.length > 0)
-                    /*  @<directive> <id> { ... }  */
-                    scopeLocal = arg
-                else
-                    /*  @<directive> <id>;  */
-                    scope = arg
-                remove.push(node)
-            }
-            else if (typeof node === "object" && node.type === "atrule" && node.name === options.atDirectiveEsc) {
-                /*  rename escaped directive to non-escaped directive  */
-                node.name = options.atDirective
-            }
-            else if (typeof node === "object" && node.type === "rule" && scope !== "none") {
-                /*  <selector> { ... }  */
-                node.selector = postcssSelectorParser((node) => {
-                    node.walk((node /*, index */) => {
-                        if (node.type === "selector") {
-                            if (node.parent && node.parent.type === "pseudo" && node.parent.value !== ":not")
-                                return
-                            const mkAttr = () =>
-                                postcssSelectorParser.attribute({ attribute: `${options.attrPrefix}${scope}` })
-                            let insertBefore = []
-                            let handled = false
-                            node.each((subnode) => {
-                                if (subnode.type === "pseudo") {
-                                    if (!handled) {
-                                        insertBefore.push(subnode)
-                                        handled = true
+            /*  walk down a single CSS node  */
+            const walkNode = (node, scope) => {
+                var scopeLocal = scope
+                if (typeof node === "object" && node.type === "atrule" && node.name === options.atDirective) {
+                    /*  @<directive> <id>  */
+                    var arg = node.params.replace(/^["']/, "").replace(/['"]$/, "")
+                    if (arg === "")
+                        throw node.error(`postcss-scope: WARNING: missing scope argument on directive @${options.atDirective}`)
+                    if (typeof node.nodes === "object" && node.nodes instanceof Array && node.nodes.length > 0)
+                        /*  @<directive> <id> { ... }  */
+                        scopeLocal = arg
+                    else
+                        /*  @<directive> <id>;  */
+                        scope = arg
+                    remove.push(node)
+                }
+                else if (typeof node === "object" && node.type === "atrule" && node.name === options.atDirectiveEsc) {
+                    /*  rename escaped directive to non-escaped directive  */
+                    node.name = options.atDirective
+                }
+                else if (typeof node === "object" && node.type === "rule" && scope !== "none") {
+                    /*  <selector> { ... }  */
+                    node.selector = postcssSelectorParser((node) => {
+                        node.walk((node /*, index */) => {
+                            if (node.type === "selector") {
+                                if (node.parent && node.parent.type === "pseudo" && node.parent.value !== ":not")
+                                    return
+                                const mkAttr = () =>
+                                    postcssSelectorParser.attribute({ attribute: `${options.attrPrefix}${scope}` })
+                                let insertBefore = []
+                                let handled = false
+                                node.each((subnode) => {
+                                    if (subnode.type === "pseudo") {
+                                        if (!handled) {
+                                            insertBefore.push(subnode)
+                                            handled = true
+                                        }
                                     }
-                                }
-                                else if (subnode.type === "combinator") {
-                                    if (!handled)
-                                        insertBefore.push(subnode)
-                                    handled = false
-                                }
-                            })
-                            insertBefore.forEach((subnode) => {
-                                node.insertBefore(subnode, mkAttr())
-                            })
-                            if (!handled)
-                                node.append(mkAttr())
-                        }
+                                    else if (subnode.type === "combinator") {
+                                        if (!handled)
+                                            insertBefore.push(subnode)
+                                        handled = false
+                                    }
+                                })
+                                insertBefore.forEach((subnode) => {
+                                    node.insertBefore(subnode, mkAttr())
+                                })
+                                if (!handled)
+                                    node.append(mkAttr())
+                            }
+                        })
+                    }).processSync(node.selector, { lossless: true })
+                }
+
+                /*  walk down all child nodes  */
+                if (typeof node.nodes === "object" && node.nodes instanceof Array) {
+                    node.nodes.forEach((node) => {
+                        scopeLocal = walkNode(node, scopeLocal) /*  RECURSION  */
                     })
-                }).processSync(node.selector, { lossless: true })
+                }
+                return scope
             }
+            walkNode(css, options.rootScope)
 
-            /*  walk down all child nodes  */
-            if (typeof node.nodes === "object" && node.nodes instanceof Array) {
-                node.nodes.forEach((node) => {
-                    scopeLocal = walkNode(node, scopeLocal) /*  RECURSION  */
-                })
+            /*  remove all previously processed @<directive> nodes  */
+            const depth = (node) => {
+                var depth = 0;
+                while ((node = node.parent) !== undefined)
+                    depth++;
+                return depth
             }
-            return scope
+            remove.sort((a, b) => depth(b) - depth(a)).forEach((node) => {
+                if (typeof node.nodes === "object" && node.nodes instanceof Array && node.nodes.length > 0) {
+                    var anchor = node
+                    var nodes = node.nodes.slice(0)
+                    nodes.forEach((node) => {
+                        node.remove()
+                        anchor.parent.insertAfter(anchor, node)
+                        anchor = node
+                    })
+                }
+                node.remove()
+            })
         }
-        walkNode(css, options.rootScope)
-
-        /*  remove all previously processed @<directive> nodes  */
-        const depth = (node) => {
-            var depth = 0;
-            while ((node = node.parent) !== undefined)
-                depth++;
-            return depth
-        }
-        remove.sort((a, b) => depth(b) - depth(a)).forEach((node) => {
-            if (typeof node.nodes === "object" && node.nodes instanceof Array && node.nodes.length > 0) {
-                var anchor = node
-                var nodes = node.nodes.slice(0)
-                nodes.forEach((node) => {
-                    node.remove()
-                    anchor.parent.insertAfter(anchor, node)
-                    anchor = node
-                })
-            }
-            node.remove()
-        })
     }
-})
+}
+
+module.exports.postcss = true
 
